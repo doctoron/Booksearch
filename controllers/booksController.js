fb3884f7ba288517bdb117021176e053ba70612b
doctoron <ron.antonio@me.com>
2019-05-01 00:23:02 -0400

    CSS changes

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -2,34 +2,34 @@ const db = require("../models");
 
 // Defining methods for the booksController
 module.exports = {
-  findAll: function(req, res) {
+  findAll: (req, res) => {
     db.Book
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  findById: function(req, res) {
+  findById: (req, res) => {
     console.log(req.params.id)
     db.Book
       .find({title: req.params.id })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  create: function(req, res) {
+  create: (req, res) => {
     console.log(req.body)
     db.Book
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  update: function(req, res) {
+  update: (req, res) => {
     db.Book
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  remove: function(req, res) {
+  remove: (req, res) => {
     db.Book
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
